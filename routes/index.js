ccb00d1aec598e28e85c8d989df147d98f175dac
Mahesh <maheshmhaske241198@gmail.com>
2023-07-01 10:22:56 +0530

    ok:

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,45 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var pdf = require('html-pdf');
-var path = require('path');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  var htmlFilePath = path.join(__dirname, 'index.html');
-  var html = fs.readFileSync(htmlFilePath, 'utf8');
-  var options = { format: 'Letter' };
+router.get('/', async function (req, res, next) {
+  try {
+    // Create a browser instance
+    const browser = await puppeteer.launch();
 
-  pdf.create(html, options).toFile('./businesscard.pdf', function (err, result) {
-    if (err) return console.log(err);
-    console.log(result); // { filename: '/app/businesscard.pdf' }
-  });
+    // Create a new page
+    const page = await browser.newPage();
+
+    // Get HTML content from HTML file
+    const htmlPath = path.join(__dirname, 'index.html');
+    const html = fs.readFileSync(htmlPath, 'utf-8');
+
+    await page.setContent(html, { waitUntil: 'domcontentloaded' });
+
+    // To reflect CSS used for screens instead of print
+    await page.emulateMediaType('screen');
+
+    // Generate the PDF
+    const pdf = await page.pdf({
+      path: 'result.pdf',
+      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+      printBackground: true,
+      format: 'A4',
+    });
+
+    // Close the browser instance
+    await browser.close();
+
+    // Send the PDF as a response to the API request
+    res.contentType("application/pdf");
+    res.send(pdf);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
