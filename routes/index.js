f46f3f43722721f8bcda7aef895c4e26fe1c3241
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-12-02 10:32:22 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,9 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  var fs = require('fs')
-  var conversion = require("phantom-html-to-pdf")();
-  conversion({ html: "<h1>Hello World</h1>" }, function (err, pdf) {
-    var output = fs.createWriteStream('target.pdf')
-    console.log(pdf.logs);
-    console.log(pdf.numberOfPages);
-    // since pdf.stream is a node.js stream you can use it
-    // to save the pdf to a file (like in this example) or to
-    // respond an http request.
-    pdf.stream.pipe(output);
-  });
+ return res.json({
+  status:1
+ })
 });
 
 module.exports = router;
